b2e4c285b7cdc951a53a1eb2a230ab729d6626bb
Rubibodadoo <128983239+Rubibodadoo@users.noreply.github.com>
2023-09-08 10:25:08 +0800

    Update app.js
    productanswer and difference answer

--- script/app.js
+++ script/app.js
@@ -3,7 +3,16 @@ function displayProduct() {
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum * secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+  document.getElementById("Productanswer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
 }
 
+function displayDifference() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum - secondNum;
+  document.getElementById("Differenceanswer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
+
+
 document.getElementById('multiplyButton').addEventListener("click", displayProduct);
+document.getElementById('subtractButton').addEventListener("click", displayDifference);
