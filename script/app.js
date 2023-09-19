c1fa31963d1ce7fe8ca0472345b1db74acaca2b1
Rubibodadoo <128983239+Rubibodadoo@users.noreply.github.com>
2023-09-19 08:12:44 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,18 +1,9 @@
-function displayProduct() {
+function displaySum() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
-  let total = firstNum * secondNum;
-  document.getElementById("Productanswer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+  let total = firstNum + secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
-function displayDifference() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
-
-  let total = firstNum - secondNum;
-  document.getElementById("Differenceanswer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
-
-
-document.getElementById('multiplyButton').addEventListener("click", displayProduct);
-document.getElementById('subtractButton').addEventListener("click", displayDifference);
+document.getElementById('sumButton').addEventListener("click", displaySum);
